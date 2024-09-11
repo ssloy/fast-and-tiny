46542e64039381dee9a693781fa314d58de873b1
Dmitry V. Sokolov <ha@haqr.eu>
2024-09-11 12:24:19 +0200

    Raytracing!

--- tiny.py
+++ tiny.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 def sphere_intersect(center, radius, ray_origin, ray_direction):
     proj = np.dot(ray_direction, center-ray_origin)
@@ -8,9 +9,23 @@ def sphere_intersect(center, radius, ray_origin, ray_direction):
         return True,point,(point-center)/radius    # we have a hit, intersection point, surface normal at the point
     return False,None,None # no intersection
 
-center,radius = np.array([6, 0, 7]), 2
-eye,ray = np.zeros(3), np.array([.5, 0, 0.866])
+def normalized(vector):
+    return vector / np.linalg.norm(vector)
 
-hit, point, normal = sphere_intersect(center, radius, eye, ray)
-print(point)
+def trace(eye, ray, depth):
+    hit,point,normal = sphere_intersect(np.array([6, 0, 7]), 2., eye, ray)
+    if hit: return np.array([1., .4, .6])
+    return ambient_color                                           # no intersection
 
+width, height, ambient_color = 640, 480, np.array([.5]*3)
+focal, azimuth  = 500, 30*np.pi/180
+maxdepth = 3
+image = np.zeros((height, width, 3))
+for i in range(height):
+    for j in range(width):
+        ray = normalized(np.array([j-width/2, i-height/2, focal]))        # emit the ray along Z axis
+        ray[0],ray[2] = (np.cos(azimuth)*ray[0] + np.sin(azimuth)*ray[2], # and then rotate it 30 degrees around Y axis
+                        -np.sin(azimuth)*ray[0] + np.cos(azimuth)*ray[2])
+        image[i, j] += trace(np.zeros(3), ray, 0)
+    print("%d/%d" % (i + 1, height))
+plt.imsave('result.png', np.clip(image, 0, 1))
