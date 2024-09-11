97ee4ca6cc9279bd21b4d0d392c2c31e58da90b7
Dmitry V. Sokolov <ha@haqr.eu>
2024-09-11 12:25:29 +0200

    multiple spheres

--- tiny.py
+++ tiny.py
@@ -9,12 +9,23 @@ def sphere_intersect(center, radius, ray_origin, ray_direction):
         return True,point,(point-center)/radius    # we have a hit, intersection point, surface normal at the point
     return False,None,None # no intersection
 
+def scene_intersect(ray_origin, ray_direction):
+    nearest = np.inf                             # the (squared) distance from the ray origin to the nearest point in the scene
+    point,normal,color = None,None,None # the information about the intersection point we want to return
+    for o in [ {'center': np.array([  6,   0,  7]), 'radius':  2, 'color': np.array([1., .4, .6])},  # description of the scene:
+               {'center': np.array([2.8, 1.1,  7]), 'radius': .9, 'color': np.array([1., 1., .3])}]: # two spheres
+
+        hit,p,n = sphere_intersect(o['center'], o['radius'], ray_origin, ray_direction)
+        if hit and (d2:=np.dot(p-ray_origin, p-ray_origin))<nearest: # we have encountered the closest point so far
+            nearest,point,normal,color = d2,p,n,o['color']
+    return nearest<np.inf, point, normal, color # hit or not, intersection point, normal at the point, color of the object
+
 def normalized(vector):
     return vector / np.linalg.norm(vector)
 
 def trace(eye, ray, depth):
-    hit,point,normal = sphere_intersect(np.array([6, 0, 7]), 2., eye, ray)
-    if hit: return np.array([1., .4, .6])
+    hit,point,normal,color = scene_intersect(eye, ray)             # find closest point along the ray
+    if hit: return color
     return ambient_color                                           # no intersection
 
 width, height, ambient_color = 640, 480, np.array([.5]*3)
